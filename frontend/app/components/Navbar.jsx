419b247c6a2543627e16dfe6749b181b8b710300
Hetvi <2hlodaya@gmail.com>
2024-03-23 06:25:33 +0530

    Frontend Changes

--- frontend/app/components/Navbar.jsx
+++ frontend/app/components/Navbar.jsx
@@ -1,24 +1,21 @@
-'use client'
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 const Nav = () => {
+    const [state, setState] = useState(false);
 
-    const [state, setState] = useState(false)
-
-    // Replace javascript:void(0) paths with your paths
     const navigation = [
         { title: "Features", path: "/" },
-        { title: "Integrations", path: "/" },
+        { title: "NEWS", path: "/" },
         { title: "Customers", path: "/" },
         { title: "Pricing", path: "/" }
-    ]
+    ];
 
     useEffect(() => {
         document.onclick = (e) => {
             const target = e.target;
             if (!target.closest(".menu-btn")) setState(false);
         };
-    }, [])
+    }, []);
 
     return (
         <nav className={`bg-white mt-5 pb-5 md:text-sm ${state ? "shadow-lg rounded-xl border mx-2 mt-2 md:shadow-none md:border-none md:mx-2 md:mt-0" : ""}`}
@@ -31,14 +28,15 @@ const Nav = () => {
         }}>
             <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
                 <div className="flex items-center justify-between py-5 md:block">
-                    <a href="/">
+                    <a href="/" style={{ display: 'block', paddingTop: '10px' }}>
                         <img
-                            src="https://www.floatui.com/logo.svg"
-                            width={120}
-                            height={50}
+                            src="logo.png"
+                            width={60}
+                            height={25}
                             alt="Float UI logo"
                         />
                     </a>
+
                     <div className="md:hidden">
                         <button className="menu-btn text-white hover:text-gray-800"
                             onClick={() => setState(!state)}
@@ -59,17 +57,13 @@ const Nav = () => {
                 </div>
                 <div className={`flex-1 items-center mt-8 md:mt-0 md:flex ${state ? 'block' : 'hidden'} `}>
                     <ul className="justify-center items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
-                        {
-                            navigation.map((item, idx) => {
-                                return (
-                                    <li key={idx} className="text-white hover:text-gray-900">
-                                        <a href={item.path} className="block">
-                                            {item.title}
-                                        </a>
-                                    </li>
-                                )
-                            })
-                        }
+                        {navigation.map((item, idx) => (
+                            <li key={idx} className="text-white hover:text-gray-900" style={{ fontSize: '1.2rem' }}>
+                                <a href={item.path} className="block">
+                                    {item.title}
+                                </a>
+                            </li>
+                        ))}
                     </ul>
                     <div className="flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
                         <a href="javascript:void(0)" className="block text-white hover:text-gray-900">
@@ -85,7 +79,7 @@ const Nav = () => {
                 </div>
             </div>
         </nav>
-    )
-}
+    );
+};
 
 export default Nav;
